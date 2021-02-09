652d62806aacf530abbebeba27394fb52cf09cd7
Jake Vivian <jacob@sadsoft.com>
2021-02-08 19:10:41 -0800

    add socials

---
+++ resources/views/components/socials.blade.php
@@ -0,0 +1,7 @@
+<div class="text-xl">Socials</div>
+<ul>
+    <li><a href="#">Twitter</a></li>
+    <li><a href="#">FB</a></li>
+    <li><a href="#">Instagram</a></li>
+    <li><a href="#">Pinterest</a></li>
+</ul>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,22 +1,27 @@
 <x-site-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <nav class=class="main-nav sm:flex mt-12 sm:mt-0 flex-col sm:flex-row items-center relative flex-1 justify-center">
-                <div class="text-xl flex flex-col sm:flex-row items-center relative flex-1 justify-center">
-                    <a class="block py-2 sm:py-4 sm:px-4 text-black sm:text-secondary opacity-75 hover:opacity-100" href="#">Welcome</a>
-                    <a class="block py-2 sm:py-4 sm:px-4 text-black sm:text-secondary opacity-75 hover:opacity-100" href="#">About</a>
-                    <a class="block py-2 sm:py-4 sm:px-4 text-black sm:text-secondary opacity-75 hover:opacity-100" href="#">Marketplace</a>
-                    <a class="block py-2 sm:py-4 sm:px-4 text-black sm:text-secondary opacity-75 hover:opacity-100" href="#">Contact</a>
-                </div>
-            </nav>
+    <div class="flex">
+        <div class="py-12 flex-cols ">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <nav class=class="main-nav sm:flex mt-12 sm:mt-0 flex-col sm:flex-row items-center relative flex-1 justify-center">
+                    <div class="text-xl flex flex-col sm:flex-row items-center relative flex-1 justify-center">
+                        <a class="block py-2 sm:py-4 sm:px-4 text-black sm:text-secondary opacity-75 hover:opacity-100" href="#">Welcome</a>
+                        <a class="block py-2 sm:py-4 sm:px-4 text-black sm:text-secondary opacity-75 hover:opacity-100" href="#">About</a>
+                        <a class="block py-2 sm:py-4 sm:px-4 text-black sm:text-secondary opacity-75 hover:opacity-100" href="#">Marketplace</a>
+                        <a class="block py-2 sm:py-4 sm:px-4 text-black sm:text-secondary opacity-75 hover:opacity-100" href="#">Contact</a>
+                    </div>
+                </nav>
+            </div>
+            <div class="text-xl flex flex-col sm:flex-row items-center relative flex-1 justify-center">
+                <span class="text-4xl text-black">Wendy's Garage</span>
+            </div>
+            <section class="grid mx-20">
+                <x-product-item title="Distressed High Boy (Walter)"></x-product-item>
+                <x-product-item title="Distressed Hutch (Jason)"></x-product-item>
+                <x-product-item title="Farmhouse style Hutch (Joey)"></x-product-item>
+            </section>
         </div>
-        <div class="text-xl flex flex-col sm:flex-row items-center relative flex-1 justify-center">
-            <span class="text-4xl text-black">Wendy's Garage</span>
-        </div>
-        <div class="grid mx-20">
-            <x-product-item title="Distressed High Boy (Walter)"></x-product-item>
-            <x-product-item title="Distressed Hutch (Jason)"></x-product-item>
-            <x-product-item title="Farmhouse style Hutch (Joey)"></x-product-item>
+        <div class="py-16 p-12">
+            <x-socials />
         </div>
     </div>
 </x-site-layout>
